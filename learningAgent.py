656e2c0d9aee59193c73c6318fa47532b22ee9cd
tamojit chatterjee <tamojit.chatterjee9@gmail.com>
2016-09-18 19:03:35 +0530

    Update learningAgent.py

--- learningAgent.py
+++ learningAgent.py
@@ -5,24 +5,18 @@ Created on Sun Sep  4 18:42:24 2016
 @author: Kireeti
 """
 import numpy as np
+import sys
 class LearningAgent:
-    currentState= None
-    actions=[]
-    E_cap=0.0
-    P_cap=0.0
-    epsilon=0.01
-    E_min=0
-    E_init=0
-    kLookAhead=2
     
     def __init__(self, currentState, actions, E_cap,P_cap,epsilon):
+        self.debug=True
         self.currentState=currentState
         self.actions=actions
         self.E_max=E_cap
         self.P_cap=P_cap          
         self.epsilon=epsilon
-        self.E_min = (1-0.80)*self.E_cap #depth of discharge
-        self.E_init = (0.30)*self.E_cap #30% of charge every morning
+        self.E_min = (1-0.80)*self.E_max #depth of discharge
+        self.E_init = (0.30)*self.E_max #30% of charge every morning
         
 
 
@@ -31,38 +25,48 @@ class LearningAgent:
             Calculate and return allowable action set
             Output: List of indices of allowable actions
             '''
-            energy_level = currentState[1] #1->energy level
-            lower_bound = max(self.E_min - energy_level, -self.P_cap)
-            upper_bound = min(self.E_max - energy_level, self.P_cap)
+            energy_level = currentState[1] # 1.8
+            lower_bound = max(self.E_min - energy_level, -self.P_cap) # 3.0
+            upper_bound = min(self.E_max - energy_level, self.P_cap) # -0.6
             
             max_bin = int(np.digitize(upper_bound, self.actions, right=True)) ###
-            min_bin = int(np.digitize(lower_bound, self.actions)) ###            
+            min_bin = int(np.digitize(lower_bound, self.actions, right=True)) ###            
             
             legal_actions = []
             for k in range(min_bin, max_bin):
                 legal_actions.append(k)
-            print 'Legal Action Indices',legal_actions
+            if self.debug:
+                file=open('debug.txt','a')
+                file.write('in getLegalActions in LearningAgent.py.......\n')
+                file.write("currentState = " + str(currentState) + " upper_bound = " + str(upper_bound) + "lower_bound = " + str(lower_bound) + " max_bin, min_bin = " + str((max_bin, min_bin)) + "\n") 
+                file.close()
             return legal_actions
     
     def getAction(self,episodeNumber, state, model, environment, k, gama,timeStamp):
          """returns a tupple of optimal actions , reward."""
          if k==0:
              legalActions=self.getLegalActions(self.currentState)
+             if self.debug and len(legalActions)==0:
+                file=open('debug.txt','a')
+                file.write('legalActions should not be empty in getAction in LearningAgent.py.......\n')
+                file.write(" episodeNumber = " + " " + str(episodeNumber) + " state = " + str(state) + " k =" + str(k) + " timeStamp = " + timeStamp + "\n") 
+                file.close()
              flag=0             
-             QValue=None
-             optimalAction=None
-             for action in legalActions: #this is an index
-                 if flag==0:
-                     QValue=model.predictQvalue(state,self,legalActions)  #check this
-                     optimalAction=action
-                     flag=1
-                 else:
-                     if QValue < model.predictQvalue(state,self,legalActions):
-                        QValue=model.predictQvalue(state,self,legalActions)
-                        optimalAction=action
+             Qvalues=[(legalActions[i],model.predictQvalue(state,self,[j])) for i,j in enumerate(legalActions)]
+             QValue=Qvalues[0][1]
+             optimalAction=Qvalues[0][0]
+             for a,q in Qvalues:
+                if q > QValue:
+                    optimalAction=a
+                    QValue=q 
              return [optimalAction],gama*QValue
          else:
              legalActions=self.getLegalActions(self.currentState)  #check this
+             if self.debug and len(legalActions)==0:
+                file=open('debug.txt','a')
+                file.write('legalActions should not be empty in getAction in LearningAgent.py.......\n')
+                file.write(" episodeNumber = " + " " + str(episodeNumber) + " state = " + str(state) + " k =" + str(k) + " timeStamp = " + timeStamp + "\n") 
+                file.close()
              optimalReward=None
              optimalActions=[]
              currentOPtimalAction=None
@@ -73,12 +77,11 @@ class LearningAgent:
                  if isValid:
                      continue                    
                  actionTupples,reward=self.getAction(episodeNumber,nextState, model, environment, k-1, gama, timeStamp+1)
-                 if optimalReward==None:
-                     continue
                  if optimalReward < current_reward + gama*reward:
                      optimalReward = current_reward + gama*reward
                      currentOPtimalAction = action
                      optimalActions=actionTupples
+             #print([currentOPtimalAction]+optimalActions, optimalReward)
              return [currentOPtimalAction]+optimalActions, optimalReward
                     
             
